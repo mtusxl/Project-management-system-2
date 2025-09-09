55bb902a0b7ffd0860f06276e7d2bab47d53cf84
Matvey <mateusru54063@gmail.com>
2025-09-09 18:16:06 +0300

    Added schedule for warm-up cache

--- Project_Management_System/config/celery.py
+++ Project_Management_System/config/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", "Project_Management_System.config.settings.staging"
@@ -11,6 +12,15 @@ app = Celery(
     broker=os.getenv("CELERY_BROKER_URL"),
     backend=os.getenv("CELERY_RESULT_BACKEND"),
 )
-
+app.conf.beat_schedule = {
+    "warm-project-cache": {
+        "task": "Project_Management_System.Projects.tasks.warm_project_cache",
+        "schedule": crontab(minute="0", hour="*/1"),
+    },
+    "warm-tasks-cache": {
+        "task": "Project_Management_System.Tasks.tasks.warm_tasks_cache",
+        "schedule": crontab(minute="0", hour="*/1"),
+    },
+}
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
